f4397c7ef960b10ca1e72bc172ac2dce80786ee3
Frederick Cabedoce <frederick.cabedoce@hotmail.com>
2023-04-15 11:20:46 -0400

    Spring initializr commit

---
+++ src/main/java/cabedoce/cloud/solutions/shoppingcartjava17/ShoppingCartJava17Application.java
@@ -0,0 +1,13 @@
+package cabedoce.cloud.solutions.shoppingcartjava17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartJava17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartJava17Application.class, args);
+	}
+
+}
---
+++ src/test/java/cabedoce/cloud/solutions/shoppingcartjava17/ShoppingCartJava17ApplicationTests.java
@@ -0,0 +1,13 @@
+package cabedoce.cloud.solutions.shoppingcartjava17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartJava17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
